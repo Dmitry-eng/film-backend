7cc976998b465f720da60331b8bffb2f6ca4babb
Dmitry <11dima1996@gmail.com>
2026-03-12 00:30:41 +0300

    fixed rating

---
+++ src/main/java/org/film/entity/GradeEntity.java
@@ -0,0 +1,21 @@
+package org.film.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table
+public class GradeEntity extends AbstractEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "film_id", nullable = false)
+    private FilmEntity film;
+
+    @Column(name = "grade", nullable = false)
+    private Integer grade;
+
+    @ManyToOne
+    private AccountEntity account;
+
+}
