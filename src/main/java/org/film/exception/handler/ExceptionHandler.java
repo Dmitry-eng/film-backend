1d7c4a0e89458ab6cb436f7792d4c2949c9f50e7
Dmitry <11dima1996@gmail.com>
2026-03-12 00:44:18 +0300

    fixed error message

--- src/main/java/org/film/exception/handler/ExceptionHandler.java
+++ src/main/java/org/film/exception/handler/ExceptionHandler.java
@@ -1,27 +1,22 @@
 package org.film.exception.handler;
 
-import org.apache.commons.lang3.StringUtils;
 import org.film.exception.ErrorResponse;
 import org.film.exception.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionHandler {
 
     private static final String COMMA = ", ";
-    private static final String CHAR = "'";
 
 
     @org.springframework.web.bind.annotation.ExceptionHandler(ServiceException.class)
@@ -37,12 +32,16 @@ public class ExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     public ErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        String errorMessage = Arrays.stream(Objects.requireNonNull(ex.getDetailMessageArguments()))
-                .filter(o -> o instanceof ArrayList<?>)
-                .map(o -> (ArrayList<?>) o)
-                .flatMap(Collection::stream)
-                .map(Object::toString)
-                .map(s -> s.replace(CHAR, StringUtils.EMPTY))
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        FieldError::getField,
+                        Collectors.mapping(FieldError::getDefaultMessage, Collectors.joining("; "))
+                ))
+                .entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
                 .collect(Collectors.joining(COMMA));
 
         return buildResponse(errorMessage);
